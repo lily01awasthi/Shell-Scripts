1fafd84007863a3026ec982118d1b7f1471474d3
lily01awasthi <lily01awasthi@gmail.com>
2020-01-16 15:13:55 +0545

    initial commit

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ NETWORKID=1472583695
 DATADIR="/home/lily/poa/node1"
 GENESIS="/home/lily/poa/genesis.json"
 cd
-geth init --datadir $GENESIS --port $PORT  --networkid $NETWORKID 
-geth --datadir $DATADIR --port $PORT  --networkid $NETWORKID console   
+nohup geth init --datadir $GENESIS --port $PORT  --networkid $NETWORKID 
+nohup geth --datadir $DATADIR --port $PORT  --networkid $NETWORKID console   
