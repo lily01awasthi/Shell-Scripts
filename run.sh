85ee271f04e2c41779b8d0c02d1203269ab2ccf8
lily01awasthi <47897187+lily01awasthi@users.noreply.github.com>
2020-01-16 15:42:48 +0545

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PORT=30311
-NETWORKID=1472583695
-DATADIR="/home/lily/poa/node1"
-GENESIS="/home/lily/poa/genesis.json"
-cd
-nohup geth init --datadir $GENESIS --port $PORT  --networkid $NETWORKID 
-nohup geth --datadir $DATADIR --port $PORT  --networkid $NETWORKID console   
